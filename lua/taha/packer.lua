3aaf687b00e3a8d14837117afd56ba3899bad283
MimTaha <mtahanarimany@gmail.com>
2024-01-31 12:57:25 +0330

    add lsp plugin

--- lua/taha/packer.lua
+++ lua/taha/packer.lua
@@ -65,4 +65,16 @@ return require('packer').startup(function(use)
     -- github
     use 'tpope/vim-fugitive'
     use 'junegunn/gv.vim'
+    --lsp
+    use "neovim/nvim-lspconfig"
+
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'hrsh7th/cmp-cmdline'
+    use 'hrsh7th/nvim-cmp'
+
+    use 'hrsh7th/cmp-vsnip'
+    use 'hrsh7th/vim-vsnip'
+
 end)
